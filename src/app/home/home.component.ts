b4c20e557f20a072037cb721cfe72959bf854706
eimbert <45257682+eimbert@users.noreply.github.com>
2025-12-14 16:39:23 +0100

    Validate auth token and persist session info

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
 import { LoginSuccessResponse } from '../interfaces/auth';
 import { RegisterDialogComponent } from '../register-dialog/register-dialog.component';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   loggedUser: LoginSuccessResponse | null = null;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.loggedUser = this.authService.isAuthenticated() ? this.authService.getSession() : null;
+  }
 
   openLoginDialog(): void {
     this.dialog.open<LoginDialogComponent, void, LoginSuccessResponse>(LoginDialogComponent, {
